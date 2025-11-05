af2203d11d270a9eb4fbe1c4cb683df0c97c4dbf
muh-younus <braq369@gmail.com>
2025-11-05 04:09:37 -0800

    connect backend to database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -9,9 +10,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', head='About section')
-@app.route("/login")
+@app.route("/contact")
+def contact():
+    return "My contact  Page"
+@app.route("/login",methods=['GET','POST'])
 def login():
-    return "Login Page"
+    if request.method == 'POST':
+        username=request.form['username']
+        password=request.form['password']
+        print(f"Username: {username}, Password: {password}")
+    
+    return render_template('login.html ')
 
 if __name__ == "__main__":
     app.run(debug=True)
